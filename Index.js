b5557f9b10cfedf252988c18fca3386778600c22
Jesus41844 <degraciajesus831@gmail.com>
2022-05-24 18:40:23 -0500

    actualizacion

---
+++ Index.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function () {
+
+    function showPopup(){
+        $('.popUp').addClass('show');
+        $('.popUpWarp').addClass('show')
+    }
+
+    $('.bton').click(function(){
+        $('.popUp').removeClass('show');
+        $('.popUpWarp').removeClass('show')
+    });
+
+    $('.boton').click(showPopup);
+
+//    setTimeout(showPopup, 5000)
+});*/
